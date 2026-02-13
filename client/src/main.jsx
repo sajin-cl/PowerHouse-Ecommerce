ab045c27206049a3d38c59d94d0927ca2e63a299
Sajincl-03 <sajincl222@gmail.com>
2026-02-14 03:30:34 +0530

    Install bootstrap via npm

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css'
 import router from '../src/routes/AppRoutes'
 import { createRoot } from 'react-dom/client'
