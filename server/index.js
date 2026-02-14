876c071ccbf9e3152b32013b6be2170a79dfdbd6
Sajincl-03 <sajincl222@gmail.com>
2026-02-14 20:20:57 +0530

    bug 1

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV == 'production', 
       sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'lax',
-      maxAge: 24 * 60 * 60 * 1000
+      maxAge: 10 * 60 * 60 * 1000
     }
   })
 );
